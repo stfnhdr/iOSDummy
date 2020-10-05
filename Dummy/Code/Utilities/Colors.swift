02eb49c2356cdf4ac9b9de937c710ecafabb6819
Stefan Haider <stefan.haider@upstream-mobility.at>
2020-10-05 12:32:18 +0200

    cleanup

---
+++ Dummy/Code/Utilities/Colors.swift
@@ -0,0 +1,15 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+// MARK: - Asset Catalogs
+
+// swiftlint:disable identifier_name line_length nesting type_body_length type_name
+internal enum Colors {
+  internal enum Colors {
+  }
+}
+// swiftlint:enable identifier_name line_length nesting type_body_length type_name
